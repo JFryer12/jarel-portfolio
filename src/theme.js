710272a7676446a373e55765f561054a597b7f56
Jarel Fryer <jfryer@myocv.com>
2019-04-03 23:09:26 -0500

    Initial Commit

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+const colors = {
+  background: '#FFFFFF',
+  backgroundDark: '#073C78',
+
+  primary: '#2360A5',
+  primaryLight: '#2360A5',
+  primaryDark: '#0B4A91',
+
+  secondary: '#042F60',
+  secondaryLight: '#042F60',
+  secondaryDark: '#022143',
+
+  white: '#FFF'
+};
+
+module.exports = {
+  colors,
+};
